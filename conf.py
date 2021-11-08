23d9fe0f94be3f79417c484124e9eb30b08faed6
Julieta Staryfurman <julistary@gmail.com>
2021-11-08 17:44:18 +0200

    list comprehension season

--- conf.py
+++ conf.py
@@ -1,12 +1,3 @@
-TEAMS = ['ATL', 'BOS', 'BRK', 'CHO', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM',
-         'MIA', 'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
-
-SEASONS = ['2008', '2009', '2010', '2011', '2012', '2013', '2014',
-           '2015', '2016', '2017', '2018', '2019', '2020', '2021']
-
-OLD_TEAMS = {'BRK': {'old_name': 'NJN', 'until_season': '2012'}, 'CHO': {'old_name': 'CHA', 'until_season': '2014'},
-             'NOP': {'old_name': 'NOH', 'until_season': '2013'}, 'OKC': {'old_name': 'SEA', 'until_season': '2008'}}
-
 URL_1 = "https://www.basketball-reference.com/teams/"
 URL_2 = "/"
 URL_3 = ".html"
